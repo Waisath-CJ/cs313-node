adaaccba9ffc81bc2c78e15f283e69a3c982ba70
CJ Waisath <curt.waisath.jr@outlook.com>
2019-11-30 15:53:47 -0700

    fixed issue with ajax not updating page

--- project/public/javascript/scripts.js
+++ project/public/javascript/scripts.js
@@ -1,19 +1,21 @@
 function getHomework() {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = () => {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var myObj = JSON.parse(this.responseText);
-            var txt = "<table class='table table-striped'><thead><tr><th scope='col'>Homework Title</th><th scope='col'>Homework Type</th></tr></thead><tbody>";
-
+            var txt = "<table class='table'>";
+            txt += "<thead><tr><th scope='col'>Homework Name</th><th scope='col'>Homework Type</th></tr></thead><tbody>";
             for (x in myObj) {
                 txt += "<tr><td>" + myObj[x].hwname + "</td><td>" + myObj[x].hwtype_id + "</td></tr>";
             }
-
             txt += "</tbody></table>";
             document.getElementById("hwDisplay").innerHTML = txt;
         }
     };
-    
-    xhttp.open("GET", "/getHomework", true);
-    xhttp.send();
+
+    /* var id = document.getElementById("id").value;
+	var urlParameters = "?id=" + id; */
+
+    xmlhttp.open("GET", "/getHomework", true);
+    xmlhttp.send();
 }
