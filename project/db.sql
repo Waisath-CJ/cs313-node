914e323c4793c51b070507ca6f463cf18ff6b814
CJ Waisath <curt.waisath.jr@outlook.com>
2019-12-10 14:20:08 -0700

    updated project files

--- project/db.sql
+++ project/db.sql
@@ -1,5 +1,7 @@
+DROP TABLE UserHomework;
 DROP TABLE Homework;
 DROP TABLE HomeworkType;
+DROP TABLE Users;
 
 CREATE TABLE HomeworkType (
     id              SERIAL          PRIMARY KEY,
@@ -12,9 +14,26 @@ CREATE TABLE Homework (
     hwType_id       int             REFERENCES HomeworkType(id)
 );
 
+CREATE TABLE Users (
+    id              SERIAL          PRIMARY KEY,
+    username        varchar(25)     UNIQUE,
+    password        varchar(100)
+);
+
+CREATE TABLE UserHomework (
+    id              SERIAL          PRIMARY KEY,
+    users_id        int             REFERENCES Users(id),
+    homework_id     int             REFERENCES Homework(id)
+);
+
 INSERT INTO HomeworkType (hwType) VALUES ('Assignments'), ('Quizzes'), ('Discussion Boards'), ('Projects'), ('Exams');
+
 INSERT INTO Homework (hwName, hwType_id) VALUES ('Milestone 1 Submission', 1);
 INSERT INTO Homework (hwName, hwType_id) VALUES ('Design Test', 2);
 INSERT INTO Homework (hwName, hwType_id) VALUES ('SDD Workday', 3);
 INSERT INTO Homework (hwName, hwType_id) VALUES ('Project 4 Submission', 4);
 INSERT INTO Homework (hwName, hwType_id) VALUES ('Final Exam', 5);
+
+INSERT INTO Users (username, password) VALUES ('admin', 'password');
+
+INSERT INTO UserHomework (users_id, homework_id) VALUES (1, 1), (1, 2), (1, 3), (1, 4), (1, 5);
--- project/server.js
+++ project/server.js
@@ -6,28 +6,30 @@ const pool = new Pool({connectionString: connectionString});
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.use(require('morgan')('dev'));
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+app.use(session({
+  name: 'server-session-cookie-id',
+  secret: 'my express secret',
+  saveUninitialized: true,
+  resave: true,
+  store: new FileStore()
+}));
+
 app.set('port', PORT)
+    .set('views', __dirname + '/views')
     .use(express.static(__dirname + "/public"))
     .get('/', (req,res)=>{
         res.sendFile('index.html', {root: __dirname + '/public'});
     })
-    .get('/getHomework', (req, res) => {
-        var sql = "SELECT * FROM Homework";
-        pool.query(sql, (err, result) => {
-            if (err) {
-                console.log("Error in query: ");
-                console.log(err);
-            }
-
-            console.log("Back from DB with result: ");
-            console.log(result.rows);
-            res.json(result.rows); 
-        });
-    })
+    .get('/getHomework', handleGetHomework)
     .post('/login', (req, res) => {
         var username = req.body.username;
         var password = req.body.pwd;
@@ -37,3 +39,19 @@ app.set('port', PORT)
     .listen(app.get('port'), () => {
         console.log('Listening on port: ' + app.get('port'));
     });
+
+
+/* Route Handler Functions */
+function handleGetHomework(req, res) {
+    var sql = "SELECT * FROM Homework";
+    pool.query(sql, (err, result) => {
+        if (err) {
+            console.log("Error in query: ");
+            console.log(err);
+        }
+
+        console.log("Back from DB with result: ");
+        console.log(result.rows);
+        res.json(result.rows); 
+    });
+}
