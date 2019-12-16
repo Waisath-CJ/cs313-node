0d0ae832eada3be2422c7d8bdae45d5efd3468a9
CJ Waisath <curt.waisath.jr@outlook.com>
2019-12-16 12:07:29 -0700

    updated database tables

--- project/db.sql
+++ project/db.sql
@@ -1,23 +1,21 @@
 DROP TABLE UserHomework;
 DROP TABLE Homework;
-DROP TABLE HomeworkType;
 DROP TABLE Users;
-
-CREATE TABLE HomeworkType (
-    id              SERIAL          PRIMARY KEY,
-    hwType          varchar(25)     NOT NULL
-);
 
 CREATE TABLE Homework (
     id              SERIAL          PRIMARY KEY,
     hwName          varchar(50)     NOT NULL,
-    hwType_id       int             REFERENCES HomeworkType(id)
+    hwType          varchar(25)     NOT NULL,
+    dueDate         date            NOT NULL
 );
 
 CREATE TABLE Users (
     id              SERIAL          PRIMARY KEY,
-    username        varchar(25)     UNIQUE,
-    password        varchar(100)
+    firstName       varchar(25)     NOT NULL,
+    lastName        varchar(25)     NOT NULL,
+    email           varchar(50)     NOT NULL,
+    username        varchar(25)     UNIQUE NOT NULL,
+    password        varchar(100)    NOT NULL
 );
 
 CREATE TABLE UserHomework (
@@ -26,14 +24,8 @@ CREATE TABLE UserHomework (
     homework_id     int             REFERENCES Homework(id)
 );
 
-INSERT INTO HomeworkType (hwType) VALUES ('Assignments'), ('Quizzes'), ('Discussion Boards'), ('Projects'), ('Exams');
+INSERT INTO Homework (hwName, hwType, dueDate) VALUES ('Milestone 1 Submission', 'Assignment', '2019-11-10'), ('Design Test', 'Exam', '2019-11-18'), ('SDD Workday', 'Discussion Board', '2019-11-25');
 
-INSERT INTO Homework (hwName, hwType_id) VALUES ('Milestone 1 Submission', 1);
-INSERT INTO Homework (hwName, hwType_id) VALUES ('Design Test', 2);
-INSERT INTO Homework (hwName, hwType_id) VALUES ('SDD Workday', 3);
-INSERT INTO Homework (hwName, hwType_id) VALUES ('Project 4 Submission', 4);
-INSERT INTO Homework (hwName, hwType_id) VALUES ('Final Exam', 5);
+INSERT INTO Users (firstName, lastName, email, username, password) VALUES ('admin', 'person', 'admin@me.com', 'admin', 'password');
 
-INSERT INTO Users (username, password) VALUES ('admin', 'password');
-
-INSERT INTO UserHomework (users_id, homework_id) VALUES (1, 1), (1, 2), (1, 3), (1, 4), (1, 5);
+INSERT INTO UserHomework (users_id, homework_id) VALUES (1, 1), (1, 2), (1, 3);
--- project/public/javascript/scripts.js
+++ project/public/javascript/scripts.js
@@ -4,9 +4,9 @@ function getHomework() {
         if (this.readyState == 4 && this.status == 200) {
             var myObj = JSON.parse(this.responseText);
             var txt = "<table class='table'>";
-            txt += "<thead><tr><th scope='col'>Homework Name</th><th scope='col'>Homework Type</th></tr></thead><tbody>";
+            txt += "<thead><tr><th scope='col'>Homework Name</th><th scope='col'>Homework Type</th><th scope='col'>Due Date</th></tr></thead><tbody>";
             for (x in myObj) {
-                txt += "<tr><td>" + myObj[x].hwname + "</td><td>" + myObj[x].hwtype_id + "</td></tr>";
+                txt += "<tr><td>" + myObj[x].hwname + "</td><td>" + myObj[x].hwtype + "</td><td>" + myObj[x].duedate + "</td></tr>";
             }
             txt += "</tbody></table>";
             document.getElementById("hwDisplay").innerHTML = txt;
